4de05cbbd233508263fbbec402054da774fe213a
asamasoma <amasa.amos@intentmedia.com>
2015-04-28 12:42:23 -0400

    AA: Dry up test code.

--- fluent-plugin-librato.gemspec
+++ fluent-plugin-librato.gemspec
@@ -18,7 +18,7 @@ Gem::Specification.new do |spec|
 
   spec.add_development_dependency "bundler"
   spec.add_development_dependency "rake"
-  spec.add_development_dependency "test_unit"
+  spec.add_development_dependency "test-unit"
 
   spec.add_runtime_dependency "fluentd"
   spec.add_runtime_dependency "librato-metrics"
--- test/fluent/plugin/out_librato_test.rb
+++ test/fluent/plugin/out_librato_test.rb
@@ -8,47 +8,45 @@ class LibratoOutputTest < Test::Unit::TestCase
     apikey this_is_an_api_key
   ]
 
+  def create_driver(conf)
+    d = Fluent::Test::BufferedOutputTestDriver.new(Fluent::LibratoOutput, @tag).configure(conf)
+    return d.instance, d.instance.log.out.logs
+  end
+
   setup do
     Fluent::Test.setup
     @tag = 'test'
     @time = Fluent::Engine.now
-  end
-
-  def create_driver(conf)
-    Fluent::Test::BufferedOutputTestDriver.new(Fluent::LibratoOutput, @tag).configure(conf)
+    @instance, @logs = create_driver(CONFIG)
   end
 
   def test_source_length_warning
-    d = create_driver(CONFIG)
-    d.instance.validate_fields({'source' => 'b' * 64, 'key' => 'valid_key'})
+    @instance.validate_fields({'source' => 'b' * 64, 'key' => 'valid_key'})
 
-    assert_equal 1, d.instance.log.out.logs.length
-    assert_match /\[warn\]: Source value is longer than 63 characters and will be truncated in Librato/, d.instance.log.out.logs[0]
+    assert_equal 1, @logs.length
+    assert_match /\[warn\]: Source value is longer than 63 characters and will be truncated in Librato/, @logs.first
   end 
 
   def test_source_invalid_character_warning
-    d = create_driver(CONFIG)
-    d.instance.validate_fields({'source' => 'invalid\source', 'key' => 'valid_key'})
+    @instance.validate_fields({'source' => 'invalid\source', 'key' => 'valid_key'})
 
-    assert_equal 1, d.instance.log.out.logs.length
-    assert_match /\[error\]: Source value may only contain characters 'A-Za-z0-9.:-_'/, d.instance.log.out.logs[0]
+    assert_equal 1, @logs.length
+    assert_match /\[error\]: Source value may only contain characters 'A-Za-z0-9.:-_'/, @logs.first
   end 
 
 
   def test_measurement_length_warning
-    d = create_driver(CONFIG)
-    d.instance.validate_fields({'source' => 'valid_source', 'key' => 'b' * 64})
+    @instance.validate_fields({'source' => 'valid_source', 'key' => 'b' * 64})
 
-    assert_equal 1, d.instance.log.out.logs.length
-    assert_match /\[warn\]: Measurement value is longer than 63 characters and will be truncated in Librato/, d.instance.log.out.logs[0]
+    assert_equal 1, @logs.length
+    assert_match /\[warn\]: Measurement value is longer than 63 characters and will be truncated in Librato/, @logs.first
   end
 
   def test_measurement_invalid_character_warning
-    d = create_driver(CONFIG)
-    d.instance.validate_fields({'source' => 'valid_source', 'key' => 'invalid\key'})
+    @instance.validate_fields({'source' => 'valid_source', 'key' => 'invalid\key'})
 
-    assert_equal 1, d.instance.log.out.logs.length
-    assert_match /\[error\]: Measurement value may only contain characters 'A-Za-z0-9.:-_'/, d.instance.log.out.logs[0]
+    assert_equal 1, @logs.length
+    assert_match /\[error\]: Measurement value may only contain characters 'A-Za-z0-9.:-_'/, @logs.first
   end 
 
 end
